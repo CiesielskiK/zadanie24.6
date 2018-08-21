23ad88a1de84e11af77ab24161c380f1856b3ae4
Krzysztof Ciesielski <krzysztof.ciesielski@powertech.com.pl>
2018-08-21 07:32:45 +0200

    Finish task

--- src/App.js
+++ src/App.js
@@ -1,18 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import PlayersList from './components/PlayersList/PlayersList';
+import AddPlayer from './components/AddPlayer/AddPlayer';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      players: []
+    }
+  }
+
+  onScoreUpdate = (playerIndex, scoreChange) => {
+    let  newPlayers= this.state.players.map((player, index) => {
+      if (index === playerIndex) {
+        return { ...player, score: player.score + scoreChange };
+      }
+      return player;
+    })
+    let sort = newPlayers.sort(this.compare);
+    this.setState({
+      players: sort
+    })
+  }
+  
+  compare = (a, b) => {
+    const scoreA = a.score;
+    const scoreB = b.score;
+    let comparison = 0;
+    if (scoreA > scoreB) {
+      comparison = -1;
+    } else if (scoreA < scoreB) {
+      comparison = 1;
+    }
+    return comparison;
+  }
+  
+  onPlayerAdd = (playerName) => {
+    const newPlayer = {
+      name: playerName,
+      score: 0,
+    }
+    this.setState({
+      players: [...this.state.players, newPlayer]
+    })
+  }
+  
+  onPlayerRemove = (i) => {
+    const newPlayers = this.state.players.filter(player => player.name !== this.state.players[i].name);
+    let sort = newPlayers.sort(this.compare);
+    this.setState({
+      players: sort
+    })
+  }
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <AddPlayer onPlayerAdd={this.onPlayerAdd} />
+        <PlayersList players={this.state.players} onScoreUpdate={this.onScoreUpdate} onPlayerRemove={this.onPlayerRemove} />
       </div>
     );
   }
